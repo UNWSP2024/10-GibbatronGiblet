c90acda39326ed24b159f68909dc1c0438cbdf7c
GibbatronGiblet <lbgibson@students.unwsp.edu>
2025-11-06 19:33:46 -0600

    Assignment 10

--- Employee.py
+++ Employee.py
@@ -1,10 +1,34 @@
 # Program #4 Employee Class:
-# Write a class Employee that holds the following data about an employee in attributes: name, ID number, department, and job title.
+# Write a class Employee that holds the following data about an employee in attributes:
+# name, ID number, department, and job title.
 
-# Once you have written the class, write a program that creates three Employee objects to hold the following data.
+# Once you have written the class, write a program that creates
+# three Employee objects to hold the following data.
 
 # Name	ID Number	Department	Job Title
 # Susan Meyers	47899 	Accounting	Vice President
 # Mark Jones	39119	IT	Programmer
 # Joy Rogers	81774	Manufacturing	Engineer
-# The program should store the data in the three objects, then display the data for each employee on the screen.
+# The program should store the data in the three objects, \
+# then display the data for each employee on the screen.
+
+class Employee:
+    def __init__(self, name, ID, department, job):
+        self.name = name
+        self.id = ID
+        self.department = department
+        self.job = job
+
+    def display(self):
+        intro_blurb = f'Hello I am {self.name} from {self.department} and I work as one of the {self.job}s.'
+        print(intro_blurb)
+
+def main():
+    employee1 = Employee("Susan Meyer",47899,"Accounting","Vice President")
+    employee2 = Employee("Mark Jones", 39119, "IT", "Programmer")
+    employee3 = Employee("Joy Rogers", 81774, "Manufacturing", "Engineer")
+    employee1.display()
+    employee2.display()
+    employee3.display()
+
+main()
